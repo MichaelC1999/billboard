c1f45f9af51a89f7c2ebc5107027d6b6b0344e09
Michael <michaelcarroll1999@gmail.com>
2023-08-17 23:40:01 -0300

    Landing page update

--- UI/next-app/src/app/page.tsx
+++ UI/next-app/src/app/page.tsx
@@ -9,12 +9,19 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(4),
   },
   image: {
-    maxWidth: '100%',
+    width: '100%',
     height: 'auto',
   },
   distinctSection: {
     backgroundColor: theme.palette.grey[200],
     padding: theme.spacing(4, 0),
+  },
+  whiteLink: {
+    color: '#fff',
+    '&:hover': {
+      cursor: 'pointer',
+      textDecoration: 'underline',
+    }
   },
 }));
 
@@ -33,12 +40,13 @@ const HomePage = () => {
             Duis dignissim metus nec justo vulputate, non porta felis luctus. Maecenas quis elit at dui aliquet porta.
           </Typography>
         </Box>
+
         <Box className={`${classes.section} ${classes.distinctSection}`}>
           <Grid container spacing={3}>
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12} md={4}>
               <img src="https://raw.githubusercontent.com/MichaelC1999/billboard/main/UI/next-app/src/img/Snap.png" alt="Descriptive alt text" className={classes.image} />
             </Grid>
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12} md={8}>
               <Typography variant="h4" gutterBottom>
                 Billboard Snap: How Digital Marketing Becomes Web3
               </Typography>
@@ -48,9 +56,11 @@ const HomePage = () => {
             </Grid>
           </Grid>
         </Box>
+
         <Divider className={classes.section} />
+
         <Box className={classes.section}>
-          <Typography variant="h4" component={Link} color="primary">
+          <Typography variant="h4" component={Link} className={classes.whiteLink}>
             Section 1
           </Typography>
           <Typography variant="body1" paragraph>
@@ -59,7 +69,7 @@ const HomePage = () => {
         </Box>
 
         <Box className={classes.section}>
-          <Typography variant="h4" component={Link} color="primary">
+          <Typography variant="h4" component={Link} className={classes.whiteLink}>
             Section 2
           </Typography>
           <Typography variant="body1" paragraph>
@@ -68,15 +78,13 @@ const HomePage = () => {
         </Box>
 
         <Box className={classes.section}>
-          <Typography variant="h4" component={Link} color="primary">
+          <Typography variant="h4" component={Link} className={classes.whiteLink}>
             Section 3
           </Typography>
           <Typography variant="body1" paragraph>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi auctor interdum purus, ac gravida orci tincidunt eget.
           </Typography>
         </Box>
-
-
       </Container>
     </>
   );
